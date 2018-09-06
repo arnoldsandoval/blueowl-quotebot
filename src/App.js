5a0bbfc19994c5cfbfdc22c71de0c4f7e3b10195
Arnold <arnoldsandoval@me.com>
2018-09-06 15:27:58 -0700

    Initial UI Components (#2)
    * adds classNames by Jed Watson
    
    * adds Inconsolate Google Font to our index.html
    
    * adds a WIP implementation of Button
    
    * adds WIP button group component
    
    * adds editorconfig
    
    * stub out our page contents
    
    * Update index CSS with baseline CSS vars for color, in addition to grid layout definitions
    
    * stubbed out services, yarn.lock
    
    * minor style adjustments to button and button group, adds Profile component
    
    * Add a baseline profile component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.scss'
+import ButtonGroup from './components/ButtonGroup';
+import Button from './components/Button';
+import Profile from './components/Profile';
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
+      <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Quote bot</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Profile>
+          <h1>Profile Component</h1>
+          {/* Robot Image */}
+          {/* Map Image */}
+        </Profile>
+
+        <div className="actions-quote">
+          <h2>Robot commands</h2>
+          <ButtonGroup layout="inline">
+            <Button>Read this quote</Button>
+            <Button>Show past quotes</Button>
+            <Button className="icon-lil-yoda">Yodify future quotes</Button>
+          </ButtonGroup>
+        </div>
+        <div className="quote">
+          <h2>Quote</h2>
+          <blockquote>
+            random quote
+            <footer>&ndash; Author Names</footer>
+          </blockquote>
+        </div>
+        <div className="actions-user">
+          <ButtonGroup layout="list">
+            <Button theme="danger">Lame</Button>
+            <Button theme="warning">Meh&hellip;</Button>
+            <Button theme="success">Great!</Button>
+          </ButtonGroup>
+        </div>
       </div>
     )
   }
