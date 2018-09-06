6d20ae0b68b83533112d05b6b1ff36b3fae41039
Arnold Sandoval <arnold@arnie.io>
2018-09-06 15:29:01 -0700

    break out our page in to a container of its own (Main)

---
+++ src/containers/Main/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+// TODO: Aliasing...
+import ButtonGroup from '../../components/ButtonGroup';
+import Button from '../../components/Button';
+import Profile from '../../components/Profile';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="container">
+        <header className="App-header">
+          <h1 className="App-title">Quote bot</h1>
+        </header>
+        <Profile>
+          <h1>Profile Component</h1>
+          {/* Robot Image */}
+          {/* Map Image */}
+        </Profile>
+
+        <div className="actions-quote">
+          <h2>Robot commands</h2>
+          <ButtonGroup layout="inline">
+            <Button>Read this quote</Button>
+            <Button>Show past quotes</Button>
+            <Button className="icon-lil-yoda">Yodify future quotes</Button>
+          </ButtonGroup>
+        </div>
+        <div className="quote">
+          <h2>Quote</h2>
+          <blockquote>
+            random quote
+            <footer>&ndash; Author Names</footer>
+          </blockquote>
+        </div>
+        <div className="actions-user">
+          <ButtonGroup layout="list">
+            <Button theme="danger">Lame</Button>
+            <Button theme="warning">Meh&hellip;</Button>
+            <Button theme="success">Great!</Button>
+          </ButtonGroup>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
