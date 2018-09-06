7b3f58eede9b9d78bae1c679af85ef67f8ade0a3
Arnold Sandoval <arnold@arnie.io>
2018-09-06 15:04:18 -0700

    stub out our page contents

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.scss'
+import ButtonGroup from './components/ButtonGroup';
+import Button from './components/Button';
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
+      <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Quote bot</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="profile">
+          <h1>Profile Component</h1>
+          {/* Robot Image */}
+          {/* Map Image */}
+        </div>
+
+        <div className="actions-quote">
+          <h2>Robot commands</h2>
+          <ButtonGroup layout="inline">
+            <Button>Read this quote</Button>
+            <Button>Show past quotes</Button>
+            <Button className="icon-lil-yoda">Yodify future quotes</Button>
+          </ButtonGroup>
+        </div>
+        <div className="quote">
+          <blockquote>
+            random quote
+            <footer>&ndash; Author Names</footer>
+          </blockquote>
+        </div>
+        <div className="actions-user">
+          <ButtonGroup layout="list">
+            <Button theme="danger">Lame</Button>
+            <Button theme="warning">Meh&hellip;</Button>
+            <Button theme="success">Great!</Button>
+          </ButtonGroup>
+        </div>
       </div>
     )
   }
