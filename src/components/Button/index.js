e40bbd518678756acb74f63a5f3ce909253516ec
Arnold Sandoval <arnold@arnie.io>
2018-09-06 17:21:07 -0700

    adds handleClick functionality

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
+import propTypes from 'prop-types'
 import classNames from 'classnames'
+
 import './Button.scss'
 
 class Button extends Component {
     constructor (props) {
-        super(props)
+      super(props)
 
-        this.state = {
-            isDisabled: false
-        }
+      this.state = {
+          isDisabled: false
+      }
+
+      /* NOTE: Could do this with an arrow function to preserve scope,
+       * but im partial to this approach, I like seeing all my redefined
+       * context changes in one place.
+       */
+
+      this.handleClick = this.handleClick.bind(this)
     }
+
+    handleClick () {
+      this.props.onClick()
+    }
+
     render () {
         const { children, theme, className } = this.props
         const componentClass = classNames({
@@ -19,13 +33,18 @@ class Button extends Component {
         })
 
         return (
-            <button className={componentClass}>{children}</button>
+            <button onClick={this.handleClick} className={componentClass}>{children}</button>
         )
     }
 }
 
 Button.defaultProps = {
-    theme: 'primary'
+  theme: 'primary',
+  onClick: () => { console.log('☠️ no onClick attribute is defined.') }
+}
+
+Button.propTypes = {
+  onClick: propTypes.func.isRequired
 }
 
 export default Button
